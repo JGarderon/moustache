9dc4c870f4f2b1b8d5ec9fad5f3260f7bc4fa7b5
Administrator <julien.garderon@gmail.com>
2024-06-06 22:23:39 +0200

    Unité 'raw' - ajout du support d'imbriquement

--- src/engine/resolver/statement/unit_raw.rs
+++ src/engine/resolver/statement/unit_raw.rs
@@ -9,6 +9,7 @@ pub fn resolve_unit<'a>(
 ) -> Result<(Vec<Part>, usize), InternalError> {
   let mut iter_parts = doc.stack.iter().skip(doc_position).enumerate();
   let mut block_ending_position: usize;
+  let mut i = 0; 
   loop {
     let part = match iter_parts.next() {
       Some((position, part)) => {
@@ -18,12 +19,18 @@ pub fn resolve_unit<'a>(
       None => return Err(create_internal_error!("Unfinished 'raw' statement")),
     };
     match part {
-      &Part::Statement(s, e) if doc.source[s + 2..e - 2].trim() == "endraw" => break,
+      &Part::Statement(s, e) if doc.source[s + 2..e - 2].trim() == "raw" => i += 1,
+      &Part::Statement(s, e) if doc.source[s + 2..e - 2].trim() == "endraw" => {
+        i -= 1; 
+        if i == 0 {
+          break;
+        }
+      }
       _ => (),
     }
   }
   Ok((
-    doc.stack[doc_position + 1..doc_position + block_ending_position - 1].to_vec(),
+    doc.stack[doc_position + 1..doc_position + block_ending_position ].to_vec(),
     block_ending_position,
   ))
 }
